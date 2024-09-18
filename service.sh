d3d3581e194c196c766f957dd1e54cac683ccff3
Keinta15 <40346724+Keinta15@users.noreply.github.com>
2024-09-17 22:02:08 -0400

    Update service.sh

--- service.sh
+++ service.sh
@@ -6,3 +6,11 @@ done
 
 until [ -d /sdcard ]; do sleep 1; done
 sleep 1 
+
+rm -rf /data/fonts
+rm -rf /data/data/com.google.android.gms/files/fonts/opentype/*ttf
+# pm disable com.google.android.gms/com.google.android.gms.fonts.provider.FontsProvider
+# pm disable com.google.android.gms/com.google.android.gms.fonts.update.UpdateSchedulerService
+
+# Sleep for 6 hours before running again
+sleep 21600 # 6 hours in seconds
