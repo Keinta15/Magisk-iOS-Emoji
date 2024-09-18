607b74ff5d3dde3ba1d8b83691ba7d9fbd9e9886
Keinta15 <40346724+Keinta15@users.noreply.github.com>
2024-09-17 22:47:55 -0400

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,16 +1,27 @@
 MODDIR=${0%/*}
 
+# Wait until the device has completed booting
 until [ "$(getprop sys.boot_completed)" = 1 ]; do 
   sleep 1; 
 done
 
-until [ -d /sdcard ]; do sleep 1; done
+# Wait until the /sdcard directory is available
+until [ -d /sdcard ]; do 
+  sleep 1
+done
+
+# Short pause to ensure readiness
 sleep 1 
 
+# Remove existing fonts directories
 rm -rf /data/fonts
+
+# Remove GMS font-related directories and files
 rm -rf /data/data/com.google.android.gms/files/fonts/opentype/*ttf
-# pm disable com.google.android.gms/com.google.android.gms.fonts.provider.FontsProvider
-# pm disable com.google.android.gms/com.google.android.gms.fonts.update.UpdateSchedulerService
+
+# Disable GMS font services
+pm disable com.google.android.gms/com.google.android.gms.fonts.provider.FontsProvider
+pm disable com.google.android.gms/com.google.android.gms.fonts.update.UpdateSchedulerService
 
 # Sleep for 6 hours before running again
 sleep 21600 # 6 hours in seconds
