ce2ee12ceee68c962c664d7111917f673f42e7f6
Keinta15 <40346724+Keinta15@users.noreply.github.com>
2025-01-17 18:56:40 -0400

    Updated Fonts.xml Symlink Handling
    - Updated logic to handle symlinks in fonts.xml.

--- customize.sh
+++ customize.sh
@@ -132,13 +132,12 @@ if [ -d "/data/fonts" ]; then
     ui_print "- Removed existing /data/fonts directory"
 fi
 
-  [[ -d /sbin/.core/mirror ]] && MIRRORPATH=/sbin/.core/mirror || unset MIRRORPATH
-  FONTS=/system/etc/fonts.xml
-  FONTFILES=$(sed -ne '/<family lang="und-Zsye".*>/,/<\/family>/ {s/.*<font weight="400" style="normal">\(.*\)<\/font>.*/\1/p;}' $MIRRORPATH$FONTS)
-  for font in $FONTFILES
-  do
-    ln -s /system/fonts/NotoColorEmoji.ttf $MODPATH/system/fonts/$font
-  done
+[[ -d /sbin/.core/mirror ]] && MIRRORPATH=/sbin/.core/mirror || unset MIRRORPATH
+FONTS=/system/etc/fonts.xml
+FONTFILES=$(sed -ne '/<family lang="und-Zsye".*>/,/<\/family>/ {s/.*<font weight="400" style="normal">\(.*\)<\/font>.*/\1/p;}' "$MIRRORPATH$FONTS")
+for font in $FONTFILES; do
+    ln -s /system/fonts/NotoColorEmoji.ttf "$MODPATH/system/fonts/$font"
+done
 
 
   set_perm_recursive $MODPATH 0 0 0755 0644
